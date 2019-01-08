a558e155b2138b183fac2121adcce75ca32e54a1
Ayano Nishimura <ayano.nishimura@tyffon.com>
2019-01-08 13:38:45 +0900

    initial commit

---
+++ Source/RealSenseSamples/RealSenseSamplesGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RealSenseSamplesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REALSENSESAMPLES_API ARealSenseSamplesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
